9f9c2529b588a76b617a245f9e9223171a78910b
victoriadesousarios16-svg <victoriadesousarios16@gmail.com>
2026-02-04 21:07:46 -0300

    Create logica.js

---
+++ ex001/logica.js
@@ -0,0 +1,22 @@
+var x = 5; 
+var y = 10;
+var z = 2;
+
+var resultado = (x + y) / z;
+
+console.log(`O resultado da conta é: $(resultado)`)
+
+
+var a = 5 + 5;
+var b = 10 - 2;
+var c = 4 * 3; 
+var d = 20 / 5;
+var e = 2**2;
+var f = 10 % 2;
+
+console.log(`Resultados: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}`);
+
+var media = 5;
+var status = media >= 7 ? "Aprovado" : "Reprovado";
+
+console.log(`O aluno está: ${status}`);
